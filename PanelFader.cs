58d8d24d97331a3b47014a052980129c7c658e3a
Liza <85101243+lizablp@users.noreply.github.com>
2021-12-10 15:01:08 +0100

    Add files via upload

---
+++ PanelFader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PanelFader : MonoBehaviour
+{
+    public GameObject Panel;
+    int counter;
+
+    public void showhidePanel()
+    {
+        counter++;
+        if(counter % 2 == 1)
+        {
+            Panel.gameObject.SetActive(false);
+        } else
+        {
+            Panel.gameObject.SetActive(true);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
