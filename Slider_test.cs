2d5441cb55667108f525826f970064d14c18f028
Liza <85101243+lizablp@users.noreply.github.com>
2021-12-13 16:37:05 +0100

    Add files via upload

---
+++ Slider_test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Slider_test : MonoBehaviour
+{
+    public Slider slider, slider2;
+    public float[] valeurs = new float[] {25, 56, 102, 84};
+    private int myInt;
+    
+    void Start()
+    {
+        slider.GetComponent<Slider>();
+        slider2.GetComponent<Slider>();
+        myInt = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+    public void ButtonClicked()
+    {
+        if (myInt < 4)
+        {
+            slider.value = valeurs[myInt-1];
+            Debug.Log(valeurs[myInt-1]);
+            myInt++;
+        }
+        else
+        {
+            Debug.Log("Exercice fini");
+        }
+         
+          
+     }
+        
+}
