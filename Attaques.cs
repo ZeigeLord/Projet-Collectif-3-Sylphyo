ca6814c869afc0f56c59c21009c2471ced8b2869
Liza <85101243+lizablp@users.noreply.github.com>
2022-01-12 12:17:00 +0100

    Add files via upload

---
+++ Attaques.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Nuance : Tutoriel
+{
+    public Slider referenceSlider, playerSlider;
+    public Text textField;
+    public float[] valeurs = new float[] {25, 56, 102};
+    private int myInt;
+    bool crescendo;
+    public MidiInOut midiInOut;
+    public MidiFilePlayer midiFilePlayer;
+    public string fileName;
+    public int indexOutput;
+    public MPTKEvent inputMidiEvent = null;
+    
+    void Start()
+    {
+        referenceSlider.GetComponent<Slider>();
+        playerSlider.GetComponent<Slider>();
+        textField.GetComponent<Text>();
+        midiInOut.GetComponent<MidiInOut>();
+        MidiInOut midiInOut = new MidiInOut(midiFile, file, index, input);
+        myInt = 1;
+        referenceSlider.value = valeurs[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    public void ChangeValue()
+    {
+          
+    }
+
+    public void Message_good()
+    {
+            
+    }
+
+        
+}
