3cfd5817e3b0390ce1e13b5ee15435f1b6da6f87
ZeigeLord <72706104+ZeigeLord@users.noreply.github.com>
2022-01-20 15:32:25 +0100

    Update Computing.cs

--- Computing.cs
+++ Computing.cs
@@ -1,121 +1,44 @@
+using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using MidiPlayerTK;
 
-public class Computing : MonoBehaviour
+
+public class InstrumentData : MonoBehaviour
 {
-    /// <summary>
-    /// Processes all computing and analyzing tasks
-    /// <summary>
-    
+    public string instrument;
 
-    ////////////////MEMBERS////////////////
+    public InstrumentData()
 
-    MPTKEvent midiEventCurrent, midiEventRef;
-
-    public string instrumentChosen;
-    InstrumentData( instrumentChosen ) instrumentData;
-    public int[9] fingeringCurrent;
-
-    ////////////////METHODS////////////////
-     
-    public bool AccuracyNote( MPTKEvent midiEventCurrent, MPTKEvent midiEventRef, int id )
+    public InstrumentData( string instrumentChosen )
     {
-        if( midiEventCurrent.Command==MPTKCommand.NoteOn && midiEventRef.Command==MPTKCommand.NoteOn )
+        instrument = instrumentChosen;
+
+        string dataSeparated = File.ReadAllText( Application.dataPath + "/FingeringCharts/" + instrument + ".txt" );
+
+        string[] dataLineSeparated = dataSeparated.Split( new[] { lineSeparator }, System.StringSplitOptions.None);
+
+        string[] dataTemp;
+
+        for ( int j = 0; j < 128; j++)
         {
-            if( midiEventCurrent.Value==midiEventRef.Value )
-                return true;
-            else
-                return false;
+            dataTemp = dataLineSeparated[j].Split(new[] { columnSeparator }, System.StringSplitOptions.None);
+
+            for (int i = 0; i < 9; i++)
+                fingeringChart[j, i] = int.Parse( dataTemp[i] );            //conversion en int
         }
-        else
-            if ( IntervalTuto( id ) == true || IntervalExercice( id )==true )
-                return true;
-            else
-                return false;
+                   
     }
 
-    public bool IntervalTuto( int id )
+    public int[] GetFingeringChart()
     {
-
-        switch( id )
-        {
-            case 1:
-                if( midiEventCurrent.Value == midiEventRef.Value -15 || midiEventCurrent.Value == midiEventRef.Value + 15 )
-                    return true;
-                else
-                    return false;
-            case 2:
-                if( midiEventCurrent.Value == midiEventRef.Value -10 || midiEventCurrent.Value == midiEventRef.Value + 10 )
-                    return true;
-                else
-                    return false;
-            case 3:
-                if( midiEventCurrent.Value == midiEventRef.Value -15 || midiEventCurrent.Value == midiEventRef.Value + 15 )
-                    return true;
-                else
-                    return false;
-            case 4: 
-                if( midiEventCurrent.Value == midiEventRef.Value -15 || midiEventCurrent.Value == midiEventRef.Value + 15 )
-                    return true;
-                else
-                    return false;
-            default:
-                return false;
-        }
+        fingeringChart;
     }
 
-    public bool IntervalExercice( int id )
-    {
-        switch( id )
-        {
-            case 1:
-                if( midiEventCurrent.Value == midiEventRef.Value -15 || midiEventCurrent.Value == midiEventRef.Value + 15 )
-                    return true;
-                else
-                    return false;
-            case 2:
-                if( midiEventCurrent.Value == midiEventRef.Value -8 || midiEventCurrent.Value == midiEventRef.Value + 8 )
-                    return true;
-                else
-                    return false;
-            case 3:
-                if( midiEventCurrent.Value == midiEventRef.Value -7 || midiEventCurrent.Value == midiEventRef.Value + 15 )
-                    return true;
-                else
-                    return false;
-            case 4: 
-                if( midiEventCurrent.Value == midiEventRef.Value -14 || midiEventCurrent.Value == midiEventRef.Value + 7 )
-                    return true;
-                else
-                    return false;
-            default:
-                return false;
-        }
-    }
+    private:
+        string lineSeparator = "\n";
+        string columnSeparator = "  ";
 
-    public bool Score( float score, float score_max )
-    {
-        if( score >= score_max )
-            return true;
-        else
-            return false;
-    }
-
-    public bool HighScore( float score, float score_max )
-    {
-        if ( score > score_max )
-            return true;
-        else
-            return false;
-    }
- 
-    public void MidiToFingering( midiEventCurrent ){
-        if ( midiEventCurrent.Command == MPTKCommand.NoteOn )
-        {
-            for ( int i = 0; i < 9; i++ )
-                fingeringCurrent[i] = instrumentData.GetFingeringChart[midiEventCurrent.Value, i];
-        }
-    }
+        int[128, 9] fingeringChart;
 }
