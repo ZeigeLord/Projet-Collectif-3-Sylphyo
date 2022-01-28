497110a24b0b8492628d71300d8e8db0852b071b
Liza <85101243+lizablp@users.noreply.github.com>
2022-01-28 11:08:48 +0100

    Add files via upload

--- ExercicesPanel.cs
+++ ExercicesPanel.cs
@@ -5,19 +5,24 @@ using UnityEngine.UI;
 
 public class ExercicesPanel : MonoBehaviour
 {
-    public GameObject main_panel, reglage_panel, exercice1_cat1_1, exercice2_cat1_1, exercice3_cat1_1, exercice1_cat1_2, exercice2_cat1_2, exercice3_cat1_2, exercice1_cat1_3, exercice2_cat1_3, exercice3_cat1_3, eval_cat1_panel;
+    public GameObject main_panel, reglage_panel, exercice1_cat1_1, exercice2_cat1_1, exercice3_cat1_1, exercice4_cat1_1, exercice5_cat1_1, exercice1_cat1_2, exercice2_cat1_2, exercice3_cat1_2, exercice4_cat1_2, exercice5_cat1_2, exercice1_cat1_3, exercice2_cat1_3, exercice3_cat1_3, exercice4_cat1_3, eval_cat1_panel;
     // Start is called before the first frame update
     void Start()
     {
         exercice1_cat1_1.SetActive(false);
         exercice2_cat1_1.SetActive(false);
         exercice3_cat1_1.SetActive(false);
+        exercice4_cat1_1.SetActive(false);
+        exercice5_cat1_1.SetActive(false);
         exercice1_cat1_2.SetActive(false);
         exercice2_cat1_2.SetActive(false);
         exercice3_cat1_2.SetActive(false);
+        exercice4_cat1_2.SetActive(false);
+        exercice5_cat1_2.SetActive(false);
         exercice1_cat1_3.SetActive(false);
         exercice2_cat1_3.SetActive(false);
         exercice3_cat1_3.SetActive(false);
+        exercice4_cat1_3.SetActive(false);
         eval_cat1_panel.SetActive(false);
     }
 
@@ -55,6 +60,24 @@ public class ExercicesPanel : MonoBehaviour
             case 10:
                 eval_cat1_panel.SetActive(true);
                 break;
+            case 11:
+                exercice4_cat1_1.SetActive(true);
+                break;
+            case 12:
+                exercice5_cat1_1.SetActive(true);
+                break;
+            case 13:
+                exercice4_cat1_2.SetActive(true);
+                break;
+            case 14:
+                exercice5_cat1_2.SetActive(true);
+                break;
+            case 15:
+                exercice4_cat1_3.SetActive(true);
+                break;
+            case 16:
+                reglage_panel.SetActive(true);
+                break;
             default:
                 break;
         }
@@ -66,7 +89,7 @@ public class ExercicesPanel : MonoBehaviour
         
     }
 
-    public void GoToReglages()
+ /*   public void GoToReglages()
     {
         reglage_panel.SetActive(true);
         exercice1_cat1_1.SetActive(false);
@@ -83,10 +106,10 @@ public class ExercicesPanel : MonoBehaviour
     public void GoToHome()
     {
         main_panel.SetActive(true);
-      /*  cat1_panel.SetActive(false);
+       cat1_panel.SetActive(false);
         cat2_panel.SetActive(false);
         cat2_panel.SetActive(false);
-        cat2_panel.SetActive(false);*/
+        cat2_panel.SetActive(false);
         exercice1_cat1_1.SetActive(false);
         exercice2_cat1_1.SetActive(false);
         exercice3_cat1_1.SetActive(false);
@@ -98,5 +121,5 @@ public class ExercicesPanel : MonoBehaviour
         exercice3_cat1_3.SetActive(false);
         eval_cat1_panel.SetActive(false);
 
-    }
+    }*/
 }
